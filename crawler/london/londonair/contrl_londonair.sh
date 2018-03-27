7173fe8d0d3ab64fb374f03538787b4c3adb402c
Matrixsun <ziwusun@gmail.com>
2018-03-27 15:33:43 +0800

    fix bug

--- crawler/london/londonair/contrl_londonair.sh
+++ crawler/london/londonair/contrl_londonair.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/orange/kddData/london/londonair
+cd `dirname $0`
+#cd /home/orange/kddData/london/londonair
 sh getLondonData.sh
 /home/orange/anaconda2/bin/python getCleanData.py -t today /home/orange/kddData/london/londonair/data/
--- crawler/london/weatherCaiyun/contrl.sh
+++ crawler/london/weatherCaiyun/contrl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/orange/kddData/london/weatherCaiyun
+cd `dirname $0`
+#cd /home/orange/kddData/london/weatherCaiyun
 sh  getWeath.sh
 /home/orange/anaconda2/bin/python datatranWeath.py -t  today /home/orange/kddData/london/weaHeyun/data/
