018ae53bfdd01727b6401216341a57c39d80f325
Matrixsun <ziwusun@gmail.com>
2018-03-27 15:15:25 +0800

    rm 无用的目录

--- londonair/contrl_londonair.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/orange/kddData/london/londonair
-sh getLondonData.sh
-/home/orange/anaconda2/bin/python getCleanData.py -t today /home/orange/kddData/london/londonair/data/
--- weatherCaiyun/contrl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/orange/kddData/london/weatherCaiyun
-sh  getWeath.sh
-/home/orange/anaconda2/bin/python datatranWeath.py -t  today /home/orange/kddData/london/weaHeyun/data/
